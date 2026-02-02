101363bdcc3344e026de5bda0f3130126a3356c7
10Tzu01 <nick3tan@gmail.com>
2026-02-02 16:10:10 +0700

    week01: initial setup and procedural code
    # Conflicts:
    #	.gitignore
    #	src/oop_105327_Nicholas/week1/GradingSystem.kt

--- src/oop_105327_Nicholas/week1/GradingSystem.kt
+++ src/oop_105327_Nicholas/week1/GradingSystem.kt
@@ -1,9 +1,1 @@
-package oop_105327_Nicholas.week01 // Pastikan package sesuai
-
-fun main() {
-    var name: String = "John Thor"
-    var score: Int = 80
-
-
-    println("Nama: " + name + ", Nilai: " + score)
 }
