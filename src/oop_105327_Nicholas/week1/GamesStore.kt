03511eff8a2606331a4318db54c1ea17021c1677
10Tzu01 <nick3tan@gmail.com>
2026-02-23 15:30:18 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_105327_Nicholas/week1/GamesStore.kt
+++ src/oop_105327_Nicholas/week1/GamesStore.kt
@@ -3,6 +3,13 @@ package oop_105327_Nicholas.week1
 fun main(){
     val gametitle = "Beat Saber"
     val price = "350000"
+    val finalprice = calculatediscount(price)
+}
+
+
+fun calculatediscount(price: Int): Int = if (price>=200000){
+    price - (price * 10 / 100)
+    println("You get 10% discount")
 }
 
 
