40fba32d4c7dd6967af6a014bfc2f4ec02a38010
10Tzu01 <nick3tan@gmail.com>
2026-03-02 13:53:15 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_105327_Nicholas/week5/Main.kt
@@ -0,0 +1,24 @@
+package oop_105327_Nicholas.week5
+
+fun main(){
+    val dosen1 = Dosen(nama = "Pak Alex", "0123456")
+    val admin1 = Admin(nama = "Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== Aktifitas Pegawai ===")
+    for(pegawai in daftarPegawai){
+        pegawai.bekerja()
+        when(pegawai){
+            is Dosen ->{
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+            is Admin ->{
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("-------------------------")
+    }
+}
