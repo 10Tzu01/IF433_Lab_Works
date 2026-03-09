566c6fa7bc605c0b042bda696cca48a85e4b131c
10Tzu01 <nick3tan@gmail.com>
2026-03-09 12:32:30 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_105327_Nicholas/week6/GadgeInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_105327_Nicholas.week6
+
+interface GadgeInterfaces {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif")
+    }
+}
+
+interface Phone{
+    fun turnOn(){
+        println("Sibyal seluler mencari jaringan")
+    }
+}
