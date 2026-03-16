6e1b96efd1563e476bea1ed638c0b4739bdffa48
10Tzu01 <nick3tan@gmail.com>
2026-03-16 15:08:53 +0700

    week07: implement data class for structural equality

--- src/oop_105327_Nicholas/week7/Main.kt
+++ src/oop_105327_Nicholas/week7/Main.kt
@@ -16,4 +16,17 @@ fun main(){
     val reg2 = RegularUser("Alice", 22)
     println(reg1)
     println("Sama? ${reg1 == reg2}")
+
+    println("\n===Test Data Class ===")
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+    println(reg1)
+    println("Sama? ${data1 == data2}")
+
+    val data3 = data1.copy(age = 23)
+    println("Hasil Copy: $data3")
+
+    val (userName, userAge) = data1
+    println("Destructured: $userName berumur $userAge")
+
 }
