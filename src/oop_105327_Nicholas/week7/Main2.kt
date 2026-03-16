ad16babe0f4e28879393a1eefba6742b721b25a4
10Tzu01 <nick3tan@gmail.com>
2026-03-16 16:41:59 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_105327_Nicholas/week7/Main2.kt
+++ src/oop_105327_Nicholas/week7/Main2.kt
@@ -1,6 +1,6 @@
 package oop_105327_Nicholas.week7
 
-fun Main2 {
+fun Main2(){
     GameManager.startGame()
     GameManager.startGame()
 
@@ -16,4 +16,22 @@ fun Main2 {
     println("Durability: ${starterWeapon.durability}")
 
     println()
+
+    val upgradedItem = starterWeapon.item.copy(
+        name = "Pedang Kayu Bapuk +1",
+        damage = 25
+    )
+
+    println("Setelah upgrade di Blacksmith:")
+    println("Nama: ${upgradedItem.name}")
+    println("Damage: ${upgradedItem.damage}")
+    println("Rarity: ${upgradedItem.rarity}")
+
+    println()
+
+
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(upgradedItem))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
