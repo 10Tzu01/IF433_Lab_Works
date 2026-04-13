329aa908527c8c7d3de92e5fefe45784274c396f
10Tzu01 <nick3tan@gmail.com>
2026-04-13 15:45:53 +0700

    week08: simulate unit test failure using !!

---
+++ src/oop_105327_Nicholas/week8/UserTest.kt
@@ -0,0 +1,20 @@
+package oop_105327_Nicholas.week8
+
+object DatabaseMock{
+    fun findUser(id: Int): UserProfile? {
+        return if(id == 1)UserProfile("TestUser", "test@test.com") else null
+    }
+}
+
+fun runMockUnitTest(){
+    println("\n=== Running Unit Test ===")
+    val testUser = DatabaseMock.findUser(1)
+
+    val initial = testUser!!.name.substring(0, 1)
+
+    check(initial == "T"){
+        "Test Failed! Initial is wrong."
+    }
+
+    println("Test Passed: Initial is T")
+}
