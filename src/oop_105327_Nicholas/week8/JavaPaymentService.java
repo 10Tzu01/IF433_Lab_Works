c152b36b8dcd21367ff4bc37eb6b92351fe087ad
10Tzu01 <nick3tan@gmail.com>
2026-04-13 16:06:08 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_105327_Nicholas/week8/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_105327_Nicholas.week8;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productID){
+        return "TRX-" + productID + "-SUCCESS";
+    }
+}
