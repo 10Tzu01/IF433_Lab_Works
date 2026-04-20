4504ff214a15c0e8b34c6b4e3087a2e981501c1a
10Tzu01 <nick3tan@gmail.com>
2026-04-20 14:38:53 +0700

    week09: (task) chain sortedBy and map for worst performers

--- src/oop_105327_Nicholas/week9/TradingModels.kt
+++ src/oop_105327_Nicholas/week9/TradingModels.kt
@@ -2,8 +2,8 @@ package oop_105327_Nicholas.week9
 
 data class TradeLog(
     val pair: String,
-    val posisition: String,
-    val levergae: Int,
+    val position: String,
+    val leverage: Int,
     val roe: Double,
     val status: String
 )
