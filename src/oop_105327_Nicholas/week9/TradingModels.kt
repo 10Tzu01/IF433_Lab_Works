e121b95fe3ed3fb8b6da775311700331a85d343e
10Tzu01 <nick3tan@gmail.com>
2026-04-20 14:18:10 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_105327_Nicholas/week9/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_105327_Nicholas.week9
+
+data class TradeLog(
+    val pair: String,
+    val posisition: String,
+    val levergae: Int,
+    val roe: Double,
+    val status: String
+)
