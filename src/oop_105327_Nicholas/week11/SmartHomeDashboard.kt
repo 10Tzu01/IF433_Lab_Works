ac1e392841258c660714153e39e83eeb39719e18
10Tzu01 <nick3tan@gmail.com>
2026-05-04 14:22:20 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_105327_Nicholas/week11/SmartHomeDashboard.kt
+++ src/oop_105327_Nicholas/week11/SmartHomeDashboard.kt
@@ -1,5 +1,7 @@
 package oop_105327_Nicholas.week11
 
+import oop_105327_Nicholas.week1.printreceipt
+
 fun main(){
     val homeDevice = mutableListOf<SmartDevice>()
 
@@ -9,4 +11,14 @@ fun main(){
     }.also{
         homeDevice.add(it)
     }
+
+    SmartDevice("Ezviz Outdoor", "Camera")
+        .apply{
+            isOnline = true
+            powerLoad = 12
+        }
+        .also{
+            println("(Log) Kamera terhubung")
+            homeDevice.add(it)
+        }
 }
