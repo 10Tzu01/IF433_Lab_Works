4635cd93b58d2c5628a9303c565412b0b055d679
10Tzu01 <nick3tan@gmail.com>
2026-05-18 13:17:33 +0700

    week13: implement printWriter with safe use block

--- src/oop_105327_Nicholas/week13/BasicFileIO.kt
+++ src/oop_105327_Nicholas/week13/BasicFileIO.kt
@@ -20,5 +20,15 @@ fun main(){
         println("Isi Index $index : $line")
 
     }
+
+    println("\n=== Test safe resource handling ===")
+    val safeFile = File("safe_logs.txt")
+
+    safeFile.printWriter().use { out ->
+        for (i in 1..100){
+            out.println("Safe log entry #$i: System status OK.")
+    }
+    }
+    println("100 baris log berhasil di-generate dengan sangat aman.")
 }
 
