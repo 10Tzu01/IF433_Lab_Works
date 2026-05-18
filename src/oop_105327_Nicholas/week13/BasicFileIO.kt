cdd82d0efa28a90ec450ba2cac181df37231aa6c
10Tzu01 <nick3tan@gmail.com>
2026-05-18 13:04:37 +0700

    v

--- src/oop_105327_Nicholas/week13/BasicFileIO.kt
+++ src/oop_105327_Nicholas/week13/BasicFileIO.kt
@@ -9,5 +9,15 @@ fun main(){
 
     file.appendText("Line 2: Menambahkan konfigurasi baru. \n")
     println("Teks berhasil di-append.")
+
+    println("\n=== Test Read Text ===")
+    val fullContent = file.readText()
+    println("Membaca sekaligus:\n$fullContent")
+
+    println("=== Test Read Lines ===")
+    val lines = file.readLines()
+    lines.forEachIndexed { index, line ->
+        println("Isi Index $index : $line")
+    }
 }
 
